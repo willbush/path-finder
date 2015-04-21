3b218748ced4b584aa812028cfc7d8246976173d
willbush <william.bush@utdallas.edu>
2015-04-20 22:35:09 -0500

    implement priority queue neighbors by weight

--- src/WIBU4.java
+++ src/WIBU4.java
@@ -1,2 +1,101 @@
+class Vertex {
+    int distance;
+    int id;
+
+    Vertex(int id) {
+        this.id = id;
+        distance = Integer.MAX_VALUE;
+    }
+}
+
+class Edge {
+    int vertexA, vertexB, weight;
+    Vertex neighbor;
+
+    Edge(int vertexA, int vertexB, int weight) {
+        this.vertexA = vertexA;
+        this.vertexB = vertexB;
+        this.weight = weight;
+        neighbor = new Vertex(vertexB);
+    }
+}
+
+class OutDegree {
+    OutDegree next;
+    Edge edge;
+
+    OutDegree(Edge edge) {
+        this.edge = edge;
+        next = null;
+    }
+
+    OutDegree(Edge edge, OutDegree next) {
+        this.edge = edge;
+        this.next = next;
+    }
+}
+
+class Graph {
+    // index + 1 represents the vertex number, which has an out degree for every edge
+    // it has connected to another vertex.
+    private OutDegree[] adjacencyList;
+
+    Graph(int maxNodes, int sourceVertex) {
+        adjacencyList = new OutDegree[maxNodes];
+        Edge circularEdge = new Edge(sourceVertex, sourceVertex, 0);
+        adjacencyList[sourceVertex - 1] = new OutDegree(circularEdge);
+    }
+
+    public void addEdge(Edge e) {
+        placeEdge(e.vertexA - 1, e);
+        placeEdge(e.vertexB - 1, e);
+    }
+
+    private void placeEdge(int vertex, Edge e) {
+        if (!hasNeighbors(vertex))
+            adjacencyList[vertex] = new OutDegree(e);
+        else
+            placeByWeight(vertex, e);
+    }
+
+    private boolean hasNeighbors(int vertex) {
+        return adjacencyList[vertex] != null;
+    }
+
+    private void placeByWeight(int vertex, Edge e) {
+        OutDegree current = adjacencyList[vertex];
+        OutDegree previous = current;
+        if (e.weight < current.edge.weight)
+            adjacencyList[vertex] = new OutDegree(e, current);
+        else {
+            while (edgeWeightIsGreaterThanCurrent(e, current)) {
+                previous = current;
+                current = current.next;
+            }
+            previous.next = new OutDegree(e, current);
+        }
+    }
+
+    private boolean edgeWeightIsGreaterThanCurrent(Edge e, OutDegree current) {
+        return current != null && e.weight > current.edge.weight;
+    }
+
+    public OutDegree getNeighbors(int vertex) {
+        return adjacencyList[vertex - 1];
+    }
+}
+
+class PathFinder {
+    Graph graph;
+
+    PathFinder(Graph g) {
+        graph = g;
+    }
+
+    public void printShortestPaths() {
+        System.out.println("1 1 0");
+    }
+}
+
 public class WIBU4 {
 }
--- test/pathTest.java
+++ test/pathTest.java
@@ -1,8 +1,47 @@
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
 public class pathTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+//    @Before
+//    public void arrange() {
+//
+//        System.setOut(new PrintStream(out));
+//    }
+
     @Test
-    public void canCreateClass() {
-        WIBU4 w = new WIBU4();
+    public void addedEdgesAreOrderedByWeight() {
+        Graph g = new Graph(7, 1);
+        g.addEdge(new Edge(1, 2, 2));
+        g.addEdge(new Edge(1, 4, 1));
+        g.addEdge(new Edge(2, 5, 10));
+        g.addEdge(new Edge(2, 4, 3));
+        g.addEdge(new Edge(5, 7, 6));
+        g.addEdge(new Edge(3, 1, 4));
+        g.addEdge(new Edge(3, 6, 5));
+        g.addEdge(new Edge(4, 3, 2));
+        g.addEdge(new Edge(7, 6, 1));
+        g.addEdge(new Edge(4, 5, 2));
+        g.addEdge(new Edge(4, 7, 4));
+        g.addEdge(new Edge(4, 6, 8));
+
+
+        for (int i = 0; i < 7; i++) {
+            OutDegree o = g.getNeighbors(i + 1);
+            System.out.println("neighbors of " + (i + 1));
+
+            while (o != null) {
+                System.out.println("edge from " + o.edge.vertexA + " to "
+                        + o.edge.vertexB + " with weight: " + o.edge.weight);
+                o = o.next;
+            }
+        }
     }
 }
