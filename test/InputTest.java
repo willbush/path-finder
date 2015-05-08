0f5768cb35649db6489a616683431bd44a686eb8
willbush <william.bush@utdallas.edu>
2015-05-07 19:47:22 -0500

    add unit test for final data

--- test/InputTest.java
+++ test/InputTest.java
@@ -51,6 +51,28 @@ public class InputTest {
     }
 
     @Test
+    public void canProcessInput4() throws IOException {
+        File input = new File("test/testData/p4d4in.txt");
+        InputStream in = new FileInputStream(input);
+        Main program = new Main(in);
+        program.run();
+
+        String expectedOut = getOutputString("test/testData/p4d4out.txt");
+        assertEquals(expectedOut, out.toString());
+    }
+
+    @Test
+    public void canProcessInput5() throws IOException {
+        File input = new File("test/testData/p4d5in.txt");
+        InputStream in = new FileInputStream(input);
+        Main program = new Main(in);
+        program.run();
+
+        String expectedOut = getOutputString("test/testData/p4d5out.txt");
+        assertEquals(expectedOut, out.toString());
+    }
+
+    @Test
     public void canProcessRandomCompleteGraph() {
         Random r = new Random();
         CompleteGraphGenerator cg = new CompleteGraphGenerator(r.nextInt(10) + 1); // interval [1, 10]
